c77dfab9818aac883d15c7b71fbb541193004ad2
Nickolas Piraino <kwnp17c@gmail.com>
2025-07-29 00:37:22 -0400

    adding new resume experience for lead software engineer section; navbar more intuitive and smaller size elements;

--- src/app/(routes)/experience/page.tsx
+++ src/app/(routes)/experience/page.tsx
@@ -6,13 +6,15 @@ const page = () => {
             <div className="relative p-3 m-3 w-3/5 flex flex-col gap-y-5 mt-10 pt-5 after:absolute after:bg-colour after:h-[10%] after:w-[30%] after:bottom-[-2px] after:right-[-2px] after:z-[-20]">
                 <span className="absolute inset-0 bg-contrast z-[-10]" />
                 <div className="">
-                    <h1 className="text-4xl lg:text-6xl underline text-center tracking-widest uppercase">Experience</h1>
+                    <h1 className="text-4xl lg:text-6xl underline text-center tracking-widest uppercase">
+                        Experience
+                    </h1>
                 </div>
                 <div className="relative pt-10 p-3 mx-3 my-5 flex flex-col gap-y-5 bg-contrast before:absolute before:bg-colour before:h-[30%] before:w-[20%] before:top-[-2px] before:left-[-2px] before:z-[-10] after:absolute after:bg-colour after:h-[30%] after:w-[20%] after:bottom-[-2px] after:right-[-2px] after:z-[-10]">
                     <div className="text-xl lg:text-2xl">
                         <p className="text-center">
                             June 2021 - December 2023<br />
-                            BigLab, Florida State University - Research Implementation Lead
+                            BigLab, Florida State University - Lead Software Engineer
                         </p>
                     </div>
                     <div className="text-xl lg:text-2xl">
@@ -21,16 +23,39 @@ const page = () => {
                         </p>
                     </div>
                     <div className="pb-10 text-lg lg:text-xl tracking-wide">
-                        <ul>
-                            <li>Developed a robust pipeline for database imports, querying and parsing over 4 million documents, converting XML to JSON for MongoDB integration.</li>
-                            <li>Enhanced application performance by optimizing runtime and page load speeds through efficient data retrieval techniques in PHP.</li>
-                            <li>Developed multiple search engines and improved search result accuracy by 60% using a PHP frontend and MongoDB backend.</li>
-                            <li>Extracted and designed a schema for a dataset exceeding 4 million records, utilizing multithreading and multiprocessing in Python for efficient data handling.</li>
-                            <li>Designed and deployed a custom AWS AMI from a virtual host disk, optimizing environment setup for scalable machine learning model deployment.</li>
-                            <li>Extracted hierarchical metadata from research tables for visualizations and improved data schemas.</li>
-                            <li>Prioritized efficient code writing and revisioning to ensure code quality and legibility.</li>
-                            <li>“COVIDKG.ORG - a Web-scale COVID-19 Interactive, Trustworthy Knowledge Graph, Constructed and Interrogated for Bias using Deep-Learning”, appeared in EDBT, 2023</li>
-                            <li>Led successful funding acquisition efforts by leading NSF I-Corps program presentations, articulating the technical merits and research potential of the project, resulting in a significant grant that enhanced project funding and supported ongoing research development.</li>
+                        <ul className="list-disc list-inside text-left flex flex-col gap-y-2">
+                            <li className="">
+                                {/* Developed a robust pipeline for database imports, querying and parsing over 4 million documents, converting XML to JSON for MongoDB integration. */}
+                                Engineered a MongoDB pipeline to import, query, and parse 4M+ documents, cutting data processing time by 55% and enabling scalable backend operations.
+                            </li>
+                            <li>
+                                {/* Enhanced application performance by optimizing runtime and page load speeds through efficient data retrieval techniques in PHP. */}
+                                Boosted web application performance by 50% by optimizing PHP data retrieval and minimizing page load times, enhancing user experience.
+                            </li>
+                            <li>
+                                {/* Developed multiple search engines and improved search result accuracy by 60% using a PHP frontend and MongoDB backend. */}
+                                Developed and deployed multiple search engines, increasing query result accuracy by 70% using PHP and MongoDB, enabling faster access to medical research data.
+                            </li>
+                            <li>
+                                {/* Extracted and designed a schema for a dataset exceeding 4 million records, utilizing multithreading and multiprocessing in Python for efficient data handling. */}
+                                Designed a scalable schema and processing workflow for a large dataset, leveraging Python multithreading/multiprocessing to accelerate throughput by 8x.
+                            </li>
+                            <li>
+                                {/* Designed and deployed a custom AWS AMI from a virtual host disk, optimizing environment setup for scalable machine learning model deployment. */}
+                                Built custom AWS AMIs for machine learning reducing deployment time by 40% and improving maintainability.
+                            </li>
+                            {/* <li>
+                                Extracted hierarchical metadata from research tables for visualizations and improved data schemas.
+                            </li>
+                            <li>
+                                Prioritized efficient code writing and revisioning to ensure code quality and legibility.
+                            </li>
+                            <li>
+                                “COVIDKG.ORG - a Web-scale COVID-19 Interactive, Trustworthy Knowledge Graph, Constructed and Interrogated for Bias using Deep-Learning”, appeared in EDBT, 2023
+                            </li>
+                            <li>
+                                Led successful funding acquisition efforts by leading NSF I-Corps program presentations, articulating the technical merits and research potential of the project, resulting in a significant grant that enhanced project funding and supported ongoing research development.
+                            </li> */}
                         </ul>
                     </div>
                 </div>
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,9 +3,9 @@ import { navItems } from "../resources/navItems"
 
 const Navigation = () => {
     return (
-        <nav className="fixed top-[1%] left-[50%] translate-x-[-50%] sm:top-[50%] sm:right-[2%] sm:translate-y-[-50%] sm:translate-x-[35%] z-10">
+        <nav className={`fixed flex top-[1%] left-[50%] translate-x-[-50%] sm:w-1/7 sm:fixed sm:top-[50%] sm:left-[100%] sm:-translate-x-[100%] sm:-translate-y-1/2 z-10`}>
             {/* <h1>Navigation</h1> */}
-            <ul className="group text-lg lg:text-2xl flex sm:flex-col text-center">
+            <ul className="w-full group text-lg lg:text-2xl flex sm:flex-col text-center">
                 {navItems.map((link, idx) => {
                     return (
                         <li key={idx} className="peer py-2 px-1 duration-500">
